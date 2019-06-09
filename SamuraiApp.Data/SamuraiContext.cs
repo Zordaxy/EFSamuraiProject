a6a1eb40639eeddedb7b08f278acfeea09c62de8
Alex Vovchuk <olexvov@gmail.com>
2019-06-09 16:08:49 -0400

    DB added

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Data
+{
+    class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\SQLEXPRESS; Database = SamuraiAppData; Trusted_Connection = True;");
+        }
+    }
+}
